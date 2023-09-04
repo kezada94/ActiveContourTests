b15bc758836cf7bb62eb16351d3ae7d9eeadddc6
newt <newt@newts-MacBook-Air.local>
2023-09-04 13:54:18 -0300

    updated scripts

--- other.py
+++ other.py
@@ -116,3 +116,156 @@ def read_mov_file(file_path):
 # Usage example
 mov_file_path = 'vidd.mp4'
 read_mov_file(mov_file_path)
+
+torch = []
+model = []
+data = []
+
+# Check if we can perform gpu computing
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = 'cpu'
+
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+from torch.utils.data import DataLoader
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True, 
+                            num_workers=4, 
+                            pin_memory=True, 
+                            prefetch_factor= 4)
+
+
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor= 4)
+
+
+import torch.nn as nn
+# Send the model to GPU memory
+model.to(device)
+# Wrap the model with DataParallel
+model = nn.DataParallel(model)
+
+
+# Send data to GPU memory
+data.to(device)
+GradScaler = []
+epochs =[]
+optimizer =[]
+autocast = []
+loss_fn=[]
+
+
+# Creates a GradScaler once at the beginning of training.
+scaler = torch.cuda.amp.GradScaler()
+
+for epoch in epochs:
+    for input, target in data:
+        optimizer.zero_grad()
+        # Runs the forward pass with autocasting.
+        with torch.autocast(device_type='cuda', dtype=torch.float16):
+            output = model(input)
+            loss = loss_fn(output, target)
+        # Scales loss.  Calls backward() on scaled loss to create scaled gradients.
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        # Updates the scale for next iteration.
+        scaler.update()
+DATASET_ID='dasd'
+
+dataset = load_dataset("path/to/local/loading_script", split="train")  # equivalent because the file has the same name as the directory
+dataset = load_dataset("path/to/local/loading_script", split="train[30:]")  # equivalent because the file has the same name as the directory
+dataset = load_dataset("path/to/local/loading_script", split="train[10%:30%]")  # equivalent because the file has the same name as the directory
+dynamic_transform = []
+static_transform = []
+
+from datasets import load_dataset
+
+dataset = load_dataset(DATASET_ID)
+
+dataset.map(static_transform, batched=True, batch_size=4096, num_proc=16)
+
+dataset.set_transform(dynamic_transform)
+
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import os
+
+def main():
+    dist.init_process_group("nccl") # 'nccl' for GPU - 'gloo' for CPU
+    rank = dist.get_rank() # Device ID
+    rank = int(os.environ['RANK'])
+    local_rank = int(os.environ['LOCAL_RANK'])# Local Device ID
+    world_size = int(os.environ['WORLD_SIZE'])# Total device number
+    master_addr = os.environ['MASTER_ADDR']
+
+    # // model, dataset, dataloader init
+
+    model.to(rank)
+    model = DDP(
+        model,
+        device_ids=[rank]
+    )
+
+    # // train/test loop
+
+if __name__ == "__main__":
+    main()
+
+
+
+from datasets import Dataset
+import torch.distributed
+
+dataset = load_dataset(DATASET_ID, split="train")
+
+if rank > 0:
+    torch.distributed.barrier()
+
+dataset2 = dataset.map(static_transform)
+
+if rank == 0:
+    torch.distributed.barrier()
+
+corr = torch.Tensor([correct]).to(rank)
+tot = torch.Tensor([total]).to(rank)
+dist.all_reduce(corr, op=dist.ReduceOp.SUM)
+dist.all_reduce(tot, op=dist.ReduceOp.SUM)
+
+
+def static_transform(examples):
+    examples["pixel_values"] = [image.convert("RGB").resize((32, 32)) for image in examples["image"]]
+    return examples
+
+def dynamic_transform(examples):
+    examples["pixel_values"] = [transform(image) for image in examples["pixel_values"]]
+    return examples
+
+
+
+from torch.utils.data.distributed import DistributedSampler
+
+train_sampler = DistributedSampler(dataset=dataset, shuffle=True)
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=16, sampler=train_sampler, num_workers=10)
+
+test_sampler = DistributedSampler(dataset=dataset, shuffle=False)
+test_loader  = torch.utils.data.DataLoader(dataset, batch_size=16, sampler=test_sampler, num_workers=10)
+
+
+# Training loop
+for epoch in tqdm(range(10)):
+    train_loader.sampler.set_epoch(epoch)
+    for i, data in tqdm(enumerate(train_loader), leave=False):
